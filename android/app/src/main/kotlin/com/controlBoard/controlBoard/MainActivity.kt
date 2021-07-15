448e01efd4c0172d05f99762551367bb4900dcde
Hala Lk <hala.lk2001@gmail.com>
2021-07-15 13:44:14 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/controlBoard/controlBoard/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ownproject.controlBoard
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/pages/login.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:mvc_pattern/mvc_pattern.dart';
+import 'package:structurepublic/src/controler/Login.dart';
+
+import '../../generated/l10n.dart';
+import '../controler/user_controller.dart';
+import '../elements/BlockButtonWidget.dart';
+import '../helpers/app_config.dart' as config;
+import '../repository/user_repository.dart' as userRepo;
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class Loginmain extends StatefulWidget {
+  @override
+  _Loginmain createState() => _Loginmain();
+}
+
+class _Loginmain extends StateMVC<Loginmain> {
+  final _formkey = GlobalKey<FormState>();
+
+  // UserController _con;
+  LoginController _loginrController;
+
+  _Loginmain() : super(LoginController()) {
+    // _con = controller;
+    _loginrController = controller;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    Firebase.initializeApp();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login To My Account'),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16),
+        child: Form(
+            key: _formkey,
+            child: Column(
+              children: <Widget>[
+                TextFormField(
+                  controller: _loginrController.emailController,
+                  //_emailcontroller,
+                  decoration: InputDecoration(
+                    hintText: 'Email',
+                  ),
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Please Fill Email Input';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  controller: _loginrController.passwordController,
+                  decoration: InputDecoration(
+                    hintText: 'Password',
+                  ),
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Please Fill Password Input';
+                    }
+
+                    return null;
+                  },
+                ),
+                SizedBox(
+                  height: 25,
+                ),
+
+                RaisedButton(
+                  color: Colors.blue,
+                  child: Text(
+                    'Login',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onPressed: () async {
+                    if (_formkey.currentState.validate()) {
+
+                      _loginrController.logfirebase();
+                    }
+                  },
+                ),
+              ],
+            )),
+      ),
+    );
+  }
+}
